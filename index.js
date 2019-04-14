c0cc622b129a68146e605158acd2e0b5802f8c0f
Juan Carlos Jimenez <jjimenezv24@gmail.com>
2019-04-14 13:50:32 -0500

    project created

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
- 
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine','hbs')
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.render('index');
 })
+
+app.get('/nosotros', function (req, res) {
+    res.render('nosotros');
+})
+
+app.get('/productos', function (req, res) {
+    res.render('productos');
+})
+
+app.get('/contacto', function (req, res) {
+    res.render('contacto');
+})
+ 
+app.listen(3000)
+console.log('Server start at port',3000);
