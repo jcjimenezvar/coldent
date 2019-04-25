9928018394ae80c49f972e152d78fb525b4f7a74
Jimenez <jjimenez71@dxc.com>
2019-04-25 11:14:10 -0500

    Modify Index.js PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
