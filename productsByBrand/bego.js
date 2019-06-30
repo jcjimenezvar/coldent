a17e8b4d6086536d0a5cc09dffa2d3021318fb9e
Juan Carlos Jimenez <jjimenez71@dxc.com>
2019-06-30 13:17:59 -0500

    chore(master): carga imagenes bego

--- productsByBrand/bego.js
+++ productsByBrand/bego.js
@@ -1,8 +1,8 @@
 const bego = [
     {
-        title: "VITA Easyshade® V",
-        shortDescription: 'El espectrofotómetro VITA Easyshade V ha sido desarrollado para la determinación del color precisa, rápida y fiable de dientes naturales, así como de restauraciones cerámicas.',
-        imageUrl: 'assets/images/vita/1.jpg',
+        title: "",
+        shortDescription: '',
+        imageUrl: 'assets/images/bego/1.jpg',
         idModal: 1
     },
 ]
